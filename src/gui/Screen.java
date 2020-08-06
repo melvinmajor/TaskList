b557e822cd53a9406da7c9e12bc8f1b8e3dfdd87
Melvin Campos Casares <melvin.campos@outlook.be>
2020-08-06 17:21:22 +0200

    GUI window now in 'mock-up' state
    * Changed font style to Roboto
    * Reformat code
    * Add tooltip text
    * Set default table model

--- src/gui/Screen.java
+++ src/gui/Screen.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
@@ -12,6 +13,8 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTable;
 import javax.swing.JMenuBar;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
 
 @SuppressWarnings("serial")
 public class Screen extends JFrame {
@@ -28,11 +31,15 @@ public class Screen extends JFrame {
 				try {
 					Screen frame = new Screen();
 					frame.setTitle("TaskList");
+					frame.pack();
+					frame.setLocationByPlatform(true);
 					frame.setVisible(true);
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setMinimumSize(new Dimension(300, 200));
-					frame.setBounds(100,100,450,300);
+					frame.setBounds(100, 100, 450, 300);
 					frame.setSize(450, 300);
+					frame.setForeground(Color.black);
+					frame.setBackground(Color.lightGray);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,25 +51,37 @@ public class Screen extends JFrame {
 	 * Create the frame.
 	 */
 	public Screen() {
+		setFont(new Font("Roboto", Font.PLAIN, 12));
+		setForeground(Color.black);
+		setBackground(Color.lightGray);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setFont(new Font("Roboto", Font.ITALIC, 12));
+		btnNewButton.setToolTipText("Add a new task");
 		menuBar.add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("Delete");
+		btnNewButton_1.setFont(new Font("Roboto", Font.ITALIC, 12));
+		btnNewButton_1.setToolTipText("Delete a specific task");
 		menuBar.add(btnNewButton_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		table = new JTable();
+		table.setFont(new Font("Roboto", Font.PLAIN, 12));
+		table.setToolTipText("All registered tasks are listed here");
+		table.setModel(new DefaultTableModel(new Object[][] { { null }, { null }, { null }, { null }, },
+				new String[] { "Task" }));
 		GridBagConstraints gbc_table = new GridBagConstraints();
 		gbc_table.gridwidth = 2;
 		gbc_table.gridheight = 5;
